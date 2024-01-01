03b0afaa65bc7bf536b9f987488cd07a3cf94d95
Maulana Sholihin <maulanasholihinoli@gmail.com>
2024-01-01 18:40:06 +0700

    include type on devices

--- src/services/devices/devices.hooks.js
+++ src/services/devices/devices.hooks.js
@@ -1,11 +1,30 @@
 const { disablePagination } = require('feathers-hooks-common');
 
+const includeType = () => {
+  return async context => {
+    const { app } = context;
+    const sequelize = app.get('sequelizeClient');
+    const { type } = sequelize.models;
+
+    const include = [type];
+
+    Object.assign(context.params, {
+      sequelize: {
+        include,
+        raw: false
+      }
+    });
+
+    return context;
+  };
+};
 
 module.exports = {
   before: {
     all: [],
     find: [
-      disablePagination()
+      disablePagination(),
+      includeType()
     ],
     get: [],
     create: [],
