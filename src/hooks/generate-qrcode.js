dfe02c94aa187584799bfc6d01cc92003442cd13
Maulana Sholihin <maulanasholihinoli@gmail.com>
2024-01-13 21:44:29 +0700

    handle encrypt id

--- src/hooks/generate-qrcode.js
+++ src/hooks/generate-qrcode.js
@@ -1,17 +1,27 @@
 const qr = require('qrcode');
 const fs = require('fs');
 const path = require('path');
+const crypto = require('crypto');
 
 module.exports = () => {
   return async context => {
     const { result, app } = context;
     const { id } = result;
-    const qrCodeData = `${id}`;
+    const timeStamp = Date.now();
+    const fileName = `qrCode${timeStamp}.png`;
+
+    const algorithm = 'aes-256-cbc';
+    const cipher = crypto.createCipher(algorithm, 'secretKey');
+    let encryptedNumber = cipher.update(String(id), 'utf-8', 'hex');
+    encryptedNumber += cipher.final('hex');
+
+    const url = `https://lxwg0xmh-3000.asse.devtunnels.ms/inventory/${encryptedNumber}`;
+    console.log(url)
+    const qrCodeData = url;
 
     const qrCodeImageBuffer = await qr.toBuffer(qrCodeData);
 
-    const timeStamp = Date.now();
-    const fileName = `qrCode${timeStamp}.png`;
+ 
     const filePath = path.join(__dirname, '..', '..', 'public', fileName);
 
     fs.writeFileSync(filePath, qrCodeImageBuffer);
@@ -20,7 +30,6 @@ module.exports = () => {
       { image: fileName },
       { where: { id } }
     );
-    
     return context;
   };
 };
